6452b7f586a19b6ccce76b077451e23fae46fc24
Cow-Kite <91773118+Cow-Kite@users.noreply.github.com>
2022-08-09 02:06:05 +0900

    Update 백대열.java

--- 백준/Silver/14490. 백대열/백대열.java
+++ 백준/Silver/14490. 백대열/백대열.java
@@ -2,7 +2,6 @@ import java.util.*;
 import java.io.*;
 
 public class Main{
-    static int count;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), ":");

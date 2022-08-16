0c2560dc7671253f8d3c0cc481ada88361b72f5f
Cow-Kite <91773118+Cow-Kite@users.noreply.github.com>
2022-08-16 21:44:29 +0900

    Update 알고리즘 수업 － 너비 우선 탐색 1.java

--- 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -7,7 +7,6 @@ public class Main{
     static int [] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken()); //정점의 수
         M = Integer.parseInt(st.nextToken()); //간선의 수

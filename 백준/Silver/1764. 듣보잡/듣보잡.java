9641830e0907be049fe969f632114a71e664089b
Cow-Kite <91773118+Cow-Kite@users.noreply.github.com>
2022-08-19 03:49:18 +0900

    [Silver IV] Title: 듣보잡, Time: 336 ms, Memory: 26392 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int n,m;
+    static String[] arr;
+    static ArrayList<String> list=new ArrayList<>();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+
+        HashSet<String> set=new HashSet<>();
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        n=Integer.parseInt(st.nextToken());
+        m=Integer.parseInt(st.nextToken());
+
+        for(int i=0; i<n; i++) {
+            set.add(br.readLine());
+        }
+        arr=new String[m];
+        for(int i=0; i<m; i++) {
+            arr[i]=br.readLine();
+        }
+        for(String str: arr) {
+          
+            if(set.contains(str)) {
+                list.add(str);
+            }
+
+        }
+        Collections.sort(list);
+        System.out.println(list.size());
+        for(String i: list) {
+            System.out.println(i);
+        }
+    }
+
+}
